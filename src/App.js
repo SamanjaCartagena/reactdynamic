cd7d6fa107b25ea47423ce6379f9d2c154c9e474
Samanja Cartagena <c.samanja09@Pabs.local>
2026-02-22 21:12:02 -0500

    featured products added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { Home } from './components/Home';
 import { About } from './components/About';
 import { Navbar } from './components/Navbar';
 import { NoMatch } from './components/NoMatch';
+import { Products } from './components/Products';
 import { OrderSummary } from './components/OrderSummary';
+import { FeaturedProducts } from './components/FeaturedProducts';
+import { NewProducts } from './components/NewProducts';
 
 import './App.css';
 
@@ -16,6 +19,14 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} /> 
       <Route path='/order-summary' element={<OrderSummary />} />
+      <Route path='/products' element={<Products />} >
+        <Route index element={<FeaturedProducts />} />
+
+        <Route path='featured' element={<FeaturedProducts />} />
+        <Route path='new' element={<NewProducts />} />
+      </Route>
+        <Route path='featured' element={<FeaturedProducts />} />
+        <Route path='new' element={<NewProducts />} />
       <Route path='*' element={<NoMatch />} />
     </Routes>
     </>
